eeeda229970a6a8f52713037d5754a8ee370d60f
Anjan Momi <anjan@momi.ca>
2018-11-11 14:38:19 -0800

    fixed access_token and TokenType not being parsed

--- config.go
+++ config.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"fmt"
+	"os"
+	"strings"
 )
 
 type ClientStruct struct {
@@ -13,10 +15,12 @@ type ClientStruct struct {
     Secret string `json:"Secret"`
     APIBase string `json:"APIBase"`
     InstanceURL string `json:"InstanceURL"`
-    UserToken []struct {
-        access_token string `json:"access_token"`
-        token_type   string `json:"token_type"`
-    } `json:"UserToken"`
+    UserToken struct {
+        Accesstoken string `json:"access_token"`
+	CreatedAt   int64  `json:"created_at"`
+	Scope       string `json:"scope"`
+        Tokentype   string `json:"token_type"`
+    } `json:"UserToken"` // In order to parse properly, the variable names' first char must be capitalized
 }
 
 const INSTANCE_FILE = "/home/anjan/output.json"
@@ -30,23 +34,30 @@ func setInstance(gClient *madon.Client) (err error){
 }
 
 func readInstance() (client ClientStruct){
-    content, err := ioutil.ReadFile(INSTANCE_FILE)
-    str := string(content)
-
-    fmt.Println(str)
+    jsonFile, err := os.Open(INSTANCE_FILE)
 
     if err != nil {
 	    panic(err)
     }
 
+    // defer the closing of our jsonFile so that we can parse it later on
+    defer jsonFile.Close()
+
+    content, _ := ioutil.ReadAll(jsonFile)
+
+
+    fmt.Println(string(content))
+    dec := json.NewDecoder(strings.NewReader(string(content)))
+
     var clientStruct ClientStruct
 
-    err = json.Unmarshal([]byte(str), &clientStruct)
+    err = dec.Decode(&clientStruct);
 
-//    if err != nil {
-//	    panic(err)
-//    }
+    if err != nil {
+	    panic(err)
+    }
 
 
+    fmt.Println(clientStruct)
     return clientStruct
 }
