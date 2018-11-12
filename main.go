e231c28d8bbe0608869534c82d6588eebedec562
Anjan Momi <anjan@momi.ca>
2018-11-11 16:41:49 -0800

    added timeline getter

--- main.go
+++ main.go
@@ -5,11 +5,14 @@ import (
 	 // "github.com/therecipe/qt/core"
 	// "github.com/therecipe/qt/uitools"
 	"github.com/therecipe/qt/widgets"
+	"fmt"
 )
 
 const APPNAME string = "fedi-go"
 const APPWEBSITE string = "momi.ca"
 const CONFIG_PATH string = "~/.config/fedi-go"
+
+
 
 func main() {
     widgets.NewQApplication(len(os.Args), os.Args)
@@ -20,8 +23,18 @@ func main() {
     client := readInstance()
 
     // gClient is output
-    _ = getAuthResume(client)
+    gClient := getAuthResume(client)
 
+//    var status madon.PostStatusParams
+//    status.Text = "Hello world"
+//    _, err := gClient.PostStatus(status)
+//    if err != nil {
+//    }
+//
+//    fmt.Println(status)
+
+    statuses := timelineGetter(gClient)
+    fmt.Println(statuses)
 
     widgets.QApplication_Exec()
 }
