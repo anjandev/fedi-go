dce77b061f860a050393bcd082888c3afe7c0073
Anjan Momi <anjan@momi.ca>
2018-11-12 03:09:58 -0800

    Started adding images

--- mainActivity.go
+++ mainActivity.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"github.com/therecipe/qt/core"
+	"github.com/therecipe/qt/webengine"
 	"github.com/therecipe/qt/uitools"
 	"github.com/therecipe/qt/widgets"
        "github.com/McKael/madon"
+	"regexp"
+	"fmt"
 )
 
 func mainActivity(statuses []madon.Status) *widgets.QWidget {
@@ -24,13 +27,45 @@ func mainActivity(statuses []madon.Status) *widgets.QWidget {
 
     var layout = widgets.NewQVBoxLayout()
     layout.AddWidget(formWidget, 0, 0)
+//	layout.AddStretch(0)
     widget.SetLayout(layout)
 
-    for i := 0; i < 20; i++{
+    for i := 0; i < len(statuses); i++{
 	post := widgets.NewQTextBrowser(nil)
-	post.SetText("By: " + statuses[i].Account.Username + statuses[i].Content)
+	contentInPost := "<p>By: " + statuses[i].Account.Username + "</p>\n" + statuses[i].Content
+
+	post.SetHtml(contentInPost)
+
+	// find number of lines to properly size box
+	// I really dont like this because if someone posts a really long 1 liner, this algorithm
+	// fails. Try something else. Check line size?
+	paras:= regexp.MustCompile("</p>")
+	matches := paras.FindAllStringIndex(contentInPost, -1)
+	const HEIGHT_OF_LINE = 25
+	fmt.Println(len(matches))
+	    post.SetMinimumHeight((len(matches)+2) * HEIGHT_OF_LINE)
+	    // minimum
+	// post.SetSizePolicy2(1, 0)
+	    // post.AdjustSize()
+	    // adjust to contents
+	// post.SetSizeAdjustPolicy(2)
 
 	ui_posts.InsertWidget(i, post, 0,0)
+
+	if len(statuses[i].MediaAttachments) > 0 {
+	    image := webengine.NewQWebEngineView(nil)
+	    fmt.Println(statuses[i].MediaAttachments[0].URL)
+	    image.SetHtml("<!DOCTYPE html> <html> <body> <img src=" + statuses[i].MediaAttachments[0].URL + "> </body> <html> ", core.NewQUrl())
+		image.SetMinimumHeight(100)
+	    // remove this. This wont print all statuses
+	    i = i +1
+	    ui_posts.InsertWidget(i, image, 0,0)
+	}
+//	for i2 := 0; i2 < len(statuses[i].MediaAttachments); i++ {
+//	    img := widgets.NewQWebEngineView(nil)
+//	    url = statuses[i].MediaAttachments[i].URL
+//	    webEngineView
+//	}
     }
 
     widget.SetWindowTitle("Fedi-go")
